64f0728591f855270faed85e7064ec68203d7a68
mohong <mohong@zmn.cn>
2024-03-20 18:09:50 +0800

    单测

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,15 +1,15 @@
 /*
  * @Author: mohong@zmn.cn
  * @Date: 2024-03-19 15:07:17
- * @LastEditTime: 2024-03-20 18:05:35
+ * @LastEditTime: 2024-03-20 18:09:21
  * @LastEditors: mohong@zmn.cn
  * @Description: 单元测试
  */
 import path from 'path';
 import apiDocs from './apiDocs.json';
 import bfmDocs from './bfmDocs.json';
-import { generateBatch as defBatch } from '../generateDefinition';
-import { generateBatch as apiBatch } from '../generateApi';
+import { generateBatch as defBatch } from '../src/generateDefinition';
+import { generateBatch as apiBatch } from '../src/generateApi';
 
 test('生成定义文件', () => {
   defBatch(bfmDocs.definitions, {
