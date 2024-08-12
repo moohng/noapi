53f99ee80049ff2724b33dbdf4c429d482181484
mohong <mohong@zmn.cn>
2024-08-12 15:03:33 +0800

    异步测试代码

--- test/noapi.test.ts
+++ test/noapi.test.ts
@@ -1,36 +1,56 @@
 import { createNoApi } from '../src/noapi';
 
-test('废弃 swagUrl 获取swagger文档', async () => {
-  const noapi = createNoApi({
-    swagUrl: 'http://192.168.91.20:8888/swagger/doc.json',
-    swagSource: '',
-  });
+// test('废弃 swagUrl 获取swagger文档', async () => {
+//   const noapi = createNoApi({
+//     swagUrl: 'http://192.168.91.20:8888/swagger/doc.json',
+//     swagSource: '',
+//   });
 
-  const apiList = await noapi.listApi();
-  expect(apiList.length).toBeGreaterThan(0);
-  expect(apiList[0].url).toBeDefined();
-});
+//   const apiList = await noapi.listApi();
+//   expect(apiList.length).toBeGreaterThan(0);
+//   expect(apiList[0].url).toBeDefined();
+// });
 
-test('通过 url 获取swagger文档', async () => {
-  const noapi = createNoApi({
-    swagUrl: 'http://192.168.91.20:8888/swagger/doc.json',
-    swagSource: 'http://192.168.91.20:8888/swagger/doc.json',
-  });
+// test('通过 url 获取swagger文档', async () => {
+//   const noapi = createNoApi({
+//     swagUrl: 'http://192.168.91.20:8888/swagger/doc.json',
+//     swagSource: 'http://192.168.91.20:8888/swagger/doc.json',
+//   });
 
-  const apiList = await noapi.listApi();
-  expect(apiList.length).toBeGreaterThan(0);
-  expect(apiList[0].url).toBeDefined();
-});
+//   const apiList = await noapi.listApi();
+//   expect(apiList.length).toBeGreaterThan(0);
+//   expect(apiList[0].url).toBeDefined();
+// });
 
 test('通过自定义来源函数获取swagger文档', async () => {
   const noapi = createNoApi({
-    swagSource: async () => {
-      const res = await fetch('http://192.168.91.20:8888/swagger/doc.json');
-      return await res.json();
-    },
+    swagSource: () => new Promise((resolve) => {
+      // 模拟异步获取数据
+      setTimeout(() => {
+        resolve({
+          swagger: '2.0',
+          info: {
+            title: '测试API',
+            version: '1.0.0',
+          },
+          paths: {
+            '/test': {
+              get: {
+                responses: {
+                  '200': {
+                    description: 'success',
+                  },
+                },
+              },
+            },
+          },
+        });
+      }, 1000);
+    }),
   });
 
   const apiList = await noapi.listApi();
-  expect(apiList.length).toBeGreaterThan(0);
-  expect(apiList[0].url).toBeDefined();
+  expect(apiList).toHaveLength(1);
+  expect(apiList[0].url).toBe('/test');
+  expect(apiList[0].method).toBe('GET');
 });
